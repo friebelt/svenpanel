e49199ff82be2bd3ab22e0c5f624f804be281824
Tobias Friebel <friebeltobias@gmail.com>
2020-03-20 13:51:48 +0100

    now plays audio, current went up to 770uA, investigating...

--- code/Nano/Nano.ino
+++ code/Nano/Nano.ino
@@ -16,8 +16,10 @@ void setup() {
   set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
   sleep_enable();
   ADCSRA = 0;  // disable ADC
+  tmrpcm.speakerPin = SPEAKER_PIN;
   Serial.begin(9600);
-  Serial.println("\n--- Svenpanel ---");
+  Serial.println("\n\n--- Svenpanel ---");
+  
 
   digitalWrite(RAIL_EN, LOW);   // enable Rail
   Serial.print("Initializing SD card...");
@@ -27,16 +29,18 @@ void setup() {
   else{
     Serial.println("Done.");
     if (SD.exists("001.wav")) {
-      Serial.println("001.wav exists.");
-      File myFile = SD.open("test.wav");
+      Serial.println("001.wav exists, attempt to play");
+      tmrpcm.play( "001.wav" );
+      for(int i=0; i<5000; i++){
+        delay(1);
+      }
+      /*File myFile = SD.open("001.wav");
       if (myFile) {
-        
-
       }
       else{
         Serial.println("error opening test.wav");
       }
-      myFile.close();
+      myFile.close();*/
       
     } else {
       Serial.println("001.wav doesn't exist.");
@@ -47,7 +51,9 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(2000);                       // wait for a second
+  delay(2000);
+  Serial.println("going to sleep");
+  delay(10);
   digitalWrite(RAIL_EN, HIGH);
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
   sleep_cpu ();
